0eee9bd45c2e6d14a3cda866ade3886a1f1ed3ad
Viagounet <ismael.rousseau@gmail.com>
2023-12-10 17:32:57 +0100

    updated agents

--- agents.py
+++ agents.py
@@ -15,10 +15,30 @@ class AgentFunction:
         return f"- {self.introspection()} ; {self.name} {self.description}"
 
     def func(self, string: str, n_times: int) -> str:
-        return string * 5
+        return string * n_times
 
     def introspection(self):
         return retrieve_function_details(self, "func").replace("func", self.name)
+
+
+class AdditionAF(AgentFunction):
+    def __init__(self, name, description) -> None:
+        super().__init__(name, description)
+
+    def func(self, a: float, b: float) -> float:
+        return a + b
+
+
+class JournalistAF(AgentFunction):
+    def __init__(self, name, description, engine) -> None:
+        self.engine = engine
+        super().__init__(name, description)
+
+    def func(self, subject: str, style: str, length: str, language: str) -> str:
+        return self.engine.query(
+            f"You're a highly skilled journalist. You're asked to write about :\nSubject: {subject}\nStyle: {style}\nLength: {length}\nLanguage: {language}",
+            1024,
+        ).content
 
 
 class Agent:
@@ -38,7 +58,9 @@ class Agent:
     def header(self):
         return f"""Your goal is to [REPLACE].
 To achieve this goal you will make good use of the following functions:
-{self.available_functions}"""
+{self.available_functions}
+
+Note: You will not make use of composite functions."""
 
     @property
     def body(self):
@@ -57,7 +79,21 @@ To achieve this goal you will make good use of the following functions:
 
     def query(self, instruction):
         prompt = self.full_prompt.replace("[REPLACE]", instruction)
-        return self.engine.query(prompt, 512)
+        raw_gpt_answer = self.engine.query(prompt, 512)
+        explaination_string = raw_gpt_answer.content.split("Action: ")[0]
+        function_string = raw_gpt_answer.content.split("Action: ")[1]
+        func_name, args = parse_function_string(function_string)
+        output = "No output"
+        for function in self.agent_functions:
+            if function.name == func_name:
+                output = function.func(*args)
+        return {
+            "raw_answer": raw_gpt_answer,
+            "explaination": explaination_string,
+            "function_used": function,
+            "arguments": args,
+            "output": output,
+        }
 
 
 def retrieve_function_details(cls, method_name):
@@ -87,12 +123,40 @@ def retrieve_function_details(cls, method_name):
     return f"{method_name}({args}) -> {ret_type}"
 
 
+import re
+import ast
+
+
+def parse_function_string(func_str):
+    # Regular expression pattern to match the function call structure
+    pattern = r"(\w+)\((.*)\)"
+    match = re.match(pattern, func_str)
+
+    if match:
+        func_name = match.group(1)
+        args_str = match.group(2)
+
+        try:
+            # Using ast.literal_eval to safely evaluate the argument string
+            args = ast.literal_eval(f"[{args_str}]")
+        except Exception as e:
+            return f"Error parsing arguments: {e}", []
+
+        return func_name, args
+    else:
+        return "Invalid function string", []
+
+
 with open("examples/parameters/philosophy.yaml", "r") as file:
     parameters = yaml.safe_load(file)
 
 engine = Engine("gpt-4", parameters=parameters)
 
 identical = AgentFunction("identical", "returns an identical string n times")
-my_agent = Agent(engine, [identical])
-agent_answer = my_agent.query("Repeat 'I love Maria' 6 times")
+addition = AdditionAF("addition", "adds two number together")
+journalist = JournalistAF(
+    "journalist", "will write a news report with great skill about any subject", engine
+)
+my_agent = Agent(engine, [journalist, identical, addition])
+agent_answer = my_agent.query("Write a report about how electricity works")
 print(agent_answer)
